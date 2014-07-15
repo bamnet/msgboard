7c3e49d4fdc1bdc4b707c1b9ced3abc25401185f
Brian Michalski <bmichalski@gmail.com>
2014-07-15 04:58:21 +0000

    Formatting cleanup.

--- page.go
+++ page.go
@@ -15,7 +15,7 @@ type Page struct {
 	Title       string
 	Content     string `datastore:",noindex"`
 	LastUpdated time.Time
-	ID string `datastore:"-"`
+	ID          string `datastore:"-"`
 }
 
 func ListPages(w http.ResponseWriter, r *http.Request) {
@@ -37,23 +37,23 @@ func ListPages(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, string(b))
 }
 
-func CreatePage(w http.ResponseWriter, r *http.Request){
+func CreatePage(w http.ResponseWriter, r *http.Request) {
 	c := appengine.NewContext(r)
 	decoder := json.NewDecoder(r.Body)
-	var p Page   
+	var p Page
 	if err := decoder.Decode(&p); err != nil {
-    http.Error(w, err.Error(), http.StatusInternalServerError)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 	p.LastUpdated = time.Now()
-	
+
 	key, err := datastore.Put(c, datastore.NewIncompleteKey(c, "Page", nil), &p)
-  if err != nil {
-    http.Error(w, err.Error(), http.StatusInternalServerError)
-    return
-  }
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 	p.ID = key.Encode()
-  b, _ := json.Marshal(p)
+	b, _ := json.Marshal(p)
 	fmt.Fprint(w, string(b))
 }
 
@@ -66,14 +66,14 @@ func GetPage(w http.ResponseWriter, r *http.Request) {
 	k, err := datastore.DecodeKey(ID)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
-    return
+		return
 	}
 	var page Page
 
 	if err := datastore.Get(c, k, &page); err != nil {
-  	http.Error(w, err.Error(), http.StatusInternalServerError)
-    return
-  }
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 	page.ID = k.Encode()
 
 	b, _ := json.Marshal(page)
