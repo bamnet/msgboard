654b169eaf5ac7639e09e654f5fa4b51bdeed4f0
Brian Michalski <bmichalski@gmail.com>
2015-05-21 23:28:48 -0400

    Add a basic ListPages test.

--- backend/page_test.go
+++ backend/page_test.go
@@ -1,7 +1,9 @@
 package msgboard
 
 import (
+	"sort"
 	"testing"
+	"time"
 
 	"appengine/aetest"
 	"appengine/datastore"
@@ -41,4 +43,50 @@ func TestGetPage(t *testing.T) {
 	if page.Title != p.Title && page.Content != p.Content {
 		t.Error("Unexpected page")
 	}
+}
+
+// ByTitle implements sort.Interface for []Page based on
+// the Title field.
+type ByTitle []Page
+
+func (a ByTitle) Len() int           { return len(a) }
+func (a ByTitle) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a ByTitle) Less(i, j int) bool { return a[i].Title < a[j].Title }
+
+func TestListPages(t *testing.T) {
+	ctx, err := aetest.NewContext(nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer ctx.Close()
+
+	p := []Page{
+		{Title: "1", Content: "Body here"},
+		{Title: "2", Content: ""},
+	}
+	_, err = datastore.PutMulti(ctx, []*datastore.Key{
+		datastore.NewIncompleteKey(ctx, "Page", nil),
+		datastore.NewIncompleteKey(ctx, "Page", nil),
+	}, p)
+	if err != nil {
+		t.Fatal(err)
+	}
+	time.Sleep(1 * time.Second)
+
+	r, err := ListPages(ctx, "")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if got, want := len(r), len(p); got != want {
+		t.Errorf("got len: %d want len: %d", got, want)
+	}
+
+	sort.Sort(ByTitle(p))
+	sort.Sort(ByTitle(r))
+	for i := range p {
+		if gotT, gotC, wantT, wantC := p[i].Title, p[i].Content, r[i].Title, r[i].Content; gotT != wantT || gotC != wantC {
+			t.Errorf("got title %s content %s, want title %s, content %s", gotT, gotC, wantT, wantC)
+		}
+	}
+
 }
